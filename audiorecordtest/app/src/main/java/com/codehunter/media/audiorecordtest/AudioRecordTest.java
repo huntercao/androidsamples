46f06982f57b4b56f5df7ac9746c059dd7defef3
huntercao <yudi.cao@gmail.com>
2016-12-16 15:51:05 +0800

    add audiorecordtest

---
+++ audiorecordtest/app/src/main/java/com/codehunter/media/audiorecordtest/AudioRecordTest.java
@@ -0,0 +1,12 @@
+package com.codehunter.media.audiorecordtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AudioRecordTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
