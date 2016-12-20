654b62e156d4d610bc39f3e7a6e8d21416c2c9f4
huntercao <yudi.cao@gmail.com>
2016-12-20 17:36:04 +0800

    Record/Playback works.

--- audiorecordtest/app/src/main/java/com/codehunter/media/audiorecordtest/AudioRecordTest.java
+++ audiorecordtest/app/src/main/java/com/codehunter/media/audiorecordtest/AudioRecordTest.java
@@ -2,19 +2,30 @@ package com.codehunter.media.audiorecordtest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+
+
+import java.io.IOException;
+
 public class AudioRecordTest extends Activity {
 
     private static String mFileName = null;
+    private static final String LOG_TAG = "AudioRecordTest";
 
+    private static final int PERMISSIONS_REQUEST_COMBINED = 101;
 
     private RecordButton mRecordButton = null;
     private MediaRecorder mRecorder = null;
@@ -25,12 +36,42 @@ public class AudioRecordTest extends Activity {
     private boolean mStartPlaying = true;
 
 
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
     class RecordButton extends Button {
         boolean mStartRecording = true;
 
         OnClickListener clicker = new OnClickListener() {
             public void onClick(View v) {
-                //onRecord(mStartRecording);
+                onRecord(mStartRecording);
                 if (mStartRecording) {
                     setText("Stop recording");
                 } else {
@@ -47,11 +88,47 @@ public class AudioRecordTest extends Activity {
         }
     }
 
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+
+            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mPlayButton.setText("Start playing");
+                    mStartPlaying = true;
+                }
+
+            });
+
+
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
     class PlayButton extends Button {
 
         OnClickListener clicker = new OnClickListener() {
             public void onClick(View v) {
-                //onPlay(mStartPlaying);
+                onPlay(mStartPlaying);
                 if (mStartPlaying) {
                     setText("Stop playing");
                 } else {
@@ -74,6 +151,30 @@ public class AudioRecordTest extends Activity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_COMBINED: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -93,5 +194,21 @@ public class AudioRecordTest extends Activity {
                         0));
 
         setContentView(ll);
+
+        if ((ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED)
+                || (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) ){
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{
+                                android.Manifest.permission.RECORD_AUDIO,
+                                android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSIONS_REQUEST_COMBINED);
+        }
+
+
     }
 }
