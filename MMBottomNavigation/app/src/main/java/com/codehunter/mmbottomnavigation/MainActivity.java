4e54b80f2deb3ba953b42ca981b9a523a0c925e2
Hunter Cao <yudi.cao@qq.com>
2019-06-23 12:08:44 +0800

    Update the strings.

--- MMBottomNavigation/app/src/main/java/com/codehunter/mmbottomnavigation/MainActivity.java
+++ MMBottomNavigation/app/src/main/java/com/codehunter/mmbottomnavigation/MainActivity.java
@@ -18,15 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            String strPage = " Page";
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(getString(R.string.title_home) + strPage);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(getString(R.string.title_dashboard) + strPage);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(getString(R.string.title_notifications) + strPage);
                     return true;
             }
             return false;
